7217645c104b0685b8adcc6fa1170c417112a661
albrox9 <dam.albrox9@gmail.com>
2022-11-10 10:47:00 +0100

    Ejercicio terminado

--- app/src/main/java/org/abrox/android_4_14/MainActivity.java
+++ app/src/main/java/org/abrox/android_4_14/MainActivity.java
@@ -5,27 +5,38 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.CheckBox;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RadioGroup rg;
+    private RadioButton rbLight;
+    private RadioButton rbNormal;
+    private RadioButton rbGraso;
+    private CheckBox ch1;
+    private CheckBox ch2;
+    private CheckBox ch3;
+    private CheckBox ch4;
+    private TextView tv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        asignarControles();
 
-        RadioGroup rg = new RadioGroup(this);
-        CheckBox ch1 = (CheckBox) findViewById(R.id.ch1);
-        CheckBox ch2 = (CheckBox) findViewById(R.id.ch2);
-        CheckBox ch3 = (CheckBox) findViewById(R.id.ch3);
-        CheckBox ch4 = (CheckBox) findViewById(R.id.ch4);
-
-        TextView tv = (TextView) findViewById(R.id.tv1);
+        configurarListeners();
 
 
 
-        rg.setOnCheckedChangeListener(
+
+
+
+
+
+        /*rg.setOnCheckedChangeListener(
                 new RadioGroup.OnCheckedChangeListener() {
                     @Override
                     public void onCheckedChanged(RadioGroup group, int checkedId) {
@@ -33,10 +44,69 @@ public class MainActivity extends AppCompatActivity {
                        sumarCalorias(checkedId, sumaTotal, tv);
                         }
                     }
-             );
+             );*/
         }
 
-    private void sumarCalorias(int checkedId, int sumaTotal, TextView tv) {
+    private void asignarControles() {
+
+        this.rg = findViewById(R.id.radioGroup);
+
+        this.rbLight = findViewById(R.id.rd1);
+        this.rbNormal = findViewById(R.id.rd2);
+        this.rbGraso = findViewById(R.id.rd3);
+
+        this.ch1 = (CheckBox) findViewById(R.id.ch1);
+        this.ch2 = (CheckBox) findViewById(R.id.ch2);
+        this.ch3 = (CheckBox) findViewById(R.id.ch3);
+        this.ch4 = (CheckBox) findViewById(R.id.ch4);
+
+        this.tv = (TextView) findViewById(R.id.tv1);
+
+
+    }
+
+    private void configurarListeners() {
+
+        this.rg.setOnCheckedChangeListener((rg, id)-> actualizar());
+
+
+        this.ch1.setOnCheckedChangeListener((cb,b) -> actualizar());
+        this.ch2.setOnCheckedChangeListener((cb,b) -> actualizar());
+        this.ch3.setOnCheckedChangeListener((cb,b) -> actualizar());
+        this.ch4.setOnCheckedChangeListener((cb,b) -> actualizar());
+
+    }
+
+    private void actualizar() {
+
+        int calorias = 0;
+        calorias += ch1.isChecked() ? 100 : 0;
+        calorias += ch2.isChecked() ? 500 : 0;
+        calorias += ch3.isChecked() ? 1500 : 0;
+        calorias += ch4.isChecked() ? 2000 : 0;
+
+        int min = 0;
+        int max = 0;
+
+
+
+        switch(rg.getCheckedRadioButtonId()){
+
+            case R.id.rd1: min= 0; max =1000;  break;
+            case R.id.rd2: min= 1000; max =2000; break;
+            case R.id.rd3: min= 2000; max =1000000000;break;
+        }
+
+        String mensaje = (calorias > min && calorias < max) ? "OK" : "FATAL";
+
+        mensaje += " te estas comiendo %1$d calorias ";
+        mensaje = String.format(mensaje, calorias);
+
+        this.tv.setText(mensaje);
+
+    }
+
+   /* private void sumarCalorias(int checkedId, int sumaTotal, TextView tv) {
 
         String message;
 
@@ -107,7 +177,7 @@ public class MainActivity extends AppCompatActivity {
 
         return contador;
 
-    }
+    }*/
 
 
 }
